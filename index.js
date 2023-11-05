6e282b8715ce461911416f366087618b33e4ce09
Macangus69 <148728673+Macangus69@users.noreply.github.com>
2023-11-05 16:19:53 -0600

    Update 2.0.4

--- index.js
+++ index.js
@@ -32,24 +32,23 @@ function setCookie(cname, cvalue, exdays) {
           cardStack.innerHTML = cardStack.innerHTML + "<div class='col'><div class='card shadow-sm'><img src='" + json['Preds'][pred]["Photo Link"] + "'><div class='card-body'><p class='card-text'>Hi, my name is " + json['Preds'][pred]["Name"] + " and I'm " + json['Preds'][pred]["Age"] + ". I'm a pred from " + json['Preds'][pred]["Lives"] + " and I work as a " + json['Preds'][pred]["Profession"] + " at " + json['Preds'][pred]["Place of Work"] + ". " + json['Preds'][pred]["Index Hook"] + ".</p><div class='d-flex justify-content-between align-items-center'><div class='btn-group'><button type='button' class='btn btn-sm btn-outline-secondary' id='" + json['Preds'][pred]["Name"] + "'data-bs-toggle='modal' data-bs-target='#staticBackdrop'>Contact!</button></div></div></div></div></div>";
         }
       }
-});
-  
-  var buttons = document.getElementsByClassName("btn-outline-secondary");
-  var checkoutList = [];
-  
-  for (var i = 0; i < buttons.length; i++) {
-    var button = buttons[i];
-    console.log(button.id);
-    button.addEventListener("click", function(e) {
-        checkoutList.push(e.target.id);
-        console.log(e.target.id);
-        setCookie("People", JSON.stringify(checkoutList), 2);
-    });
-  }
-  
-  document.getElementById("packages").addEventListener("click", function(){
-  if (checkoutList.length > 0) {
-    window.location.href = "reformationPlan.html";
-  }
-  })
-  
+      var buttons = document.getElementsByClassName("btn-outline-secondary");
+    var checkoutList = [];
+      
+    for (var i = 0; i < buttons.length; i++) {
+      var button = buttons[i];
+      console.log(button.id);
+      button.addEventListener("click", function(e) {
+          checkoutList.push(e.target.id);
+          console.log(e.target.id);
+          setCookie("People", JSON.stringify(checkoutList), 2);
+      });
+    }
+    
+    document.getElementById("packages").addEventListener("click", function(){
+    if (checkoutList.length > 0) {
+      window.location.href = "reformationPlan.html";
+    }
+    })
+    
+  });
